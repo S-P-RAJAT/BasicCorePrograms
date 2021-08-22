2cbb0a7cd555819c0ba18fdb6654ef01e32ce9cb
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-23 00:35:02 +0530

    ADDED: Program to find whether the number is even or odd

---
+++ src/com/bridgelabz/basiccoreprograms/EvenOrOdd.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class EvenOrOdd {
+public static void main(String[] args) {
+	int number = 10;
+	if (number%2 == 0) {
+		System.out.println(number+" is even!");
+	} else {
+		System.out.println(number+" is odd!");
+
+	}
+}
+}
