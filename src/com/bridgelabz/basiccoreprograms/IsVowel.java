37bf6280bfb51478320ebd1cd68f71a66477fd52
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-23 00:39:59 +0530

    ADDED: Vowel or Consonant Checking Program

---
+++ src/com/bridgelabz/basiccoreprograms/IsVowel.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class IsVowel {
+	public static void main(String[] args) {
+		char c = 'a';
+		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+			System.out.println(c + " is Vowel.");
+		} else {
+			System.out.println(c + " is Consonant.");
+		}
+	}
+}
